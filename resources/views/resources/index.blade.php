9ff92a2c505864cd14968edc0e40f13159971a47
semih-76 <semihsisman7@gmail.com>
2026-06-16 14:52:27 +0200

    feat: configure index

---
+++ resources/views/resources/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Liste des ressources');
+
+@section('content')
+    <h1>Ressources</h1>
+    @foreach($resources as $resource)
+        <div>
+            <a href="{{url('/resource/' . $resource['id']) }}">
+                {{ $resource['title'] }}
+            </a>
+            <p>{{ $resource['description'] }}</p>
+        </div>
+        @endforeach
+    @endsection
