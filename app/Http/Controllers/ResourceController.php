d269709812efddad4e2e13909590cc7c16081edc
semih-76 <semihsisman7@gmail.com>
2026-06-16 14:57:01 +0200

    feat: configure show & index functions

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class ResourceController extends Controller
 {
+    public function index() {
+        $resources = [
+            ['id' => 1, 'title' => 'Premier projet', 'description' => "Projet Laravel"],
+            ['id' => 2, 'title' => 'Deuxième projet', 'description' => "Projet React JS"]
+        ];
 
+        return view(
+            'resources.index',
+            compact('resources'));
+    }
+
+    public function show($id) {
+        $resources = [
+            ['id' => 1, 'title' => 'Premier projet', 'description' => 'Projet Laravel'],
+            ['id' => 2, 'title' => 'Deuxième projet', 'description' => 'Projet React JS'],
+        ];
+
+        // Cherche la ressource correspondant à l'id
+        $resource = collect($resources)->firstWhere('id', (int) $id);
+
+        if (!$resource) {
+            abort(404);
+        }
+        return view(
+            'resources.show', compact('resource'));
+    }
 }
