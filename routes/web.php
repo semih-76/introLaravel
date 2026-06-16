7ea4386efb2c1c8bff2c37d65c2ddc1ae76ac3c6
semih-76 <semihsisman7@gmail.com>
2026-06-16 15:14:18 +0200

    feat: configure routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ResourceController;
 
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/resources', [ResourceController::class, 'index']);
 Route::get('/resource/{resource}', [ResourceController::class, 'show']);
